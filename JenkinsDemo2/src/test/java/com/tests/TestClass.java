e03d580d5f3b42b203445e07642f6bbff74e65df
garvit.c.gupta <garvit.c.gupta@P2CLAP-52GXD63.dir.svc.accenture.com>
2022-06-22 15:39:27 +0530

    first commit

---
+++ JenkinsDemo2/src/test/java/com/tests/TestClass.java
@@ -0,0 +1,26 @@
+package com.tests;
+
+import org.testng.annotations.Test;
+
+import com.base.Baseclass;
+
+public class TestClass extends Baseclass {
+	
+	@Test
+	public void testcase1() {
+		System.out.println("This s test case 1");
+	}
+	@Test
+	public void testcase2() {
+		System.out.println("This s test case 2");
+	}
+	@Test
+	public void testcase3() {
+		System.out.println("This s test case 3");
+	}
+	@Test
+	public void testcase4() {
+		System.out.println("This s test case 4");
+	}
+
+}
